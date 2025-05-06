9ee6c5f83e1b3cae532f67f4e64a04bd25db22b4
ZhuchkaTriplesix <mrlololoshka94@gmail.com>
2025-05-06 22:23:54 +0300

    cfg + start

---
+++ api/config.py
@@ -0,0 +1,45 @@
+from abc import ABC
+from dataclasses import asdict, dataclass
+import configparser
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+
+class CfgBase(ABC):
+    dict: callable = asdict
+
+
+class PostgresCfg(CfgBase):
+    def __init__(self):
+        self.database: str = config["POSTGRES"]["DATABASE"]
+        self.driver: str = config["POSTGRES"]["DRIVER"]
+        self.database_name: str = config["POSTGRES"]["DATABASE_NAME"]
+        self.username: str = config["POSTGRES"]["USERNAME"]
+        self.password: str = config["POSTGRES"]["PASSWORD"]
+        self.ip: str = config["POSTGRES"]["IP"]
+        self.port: int = config.getint("POSTGRES", "PORT")
+
+    @property
+    def url(self) -> str:
+        return (f"{self.database}+{self.driver}://{self.username}:{self.password}@{self.ip}:{self.port}/"
+                f"{self.database_name}")
+
+
+@dataclass
+class UvicornCfg(CfgBase):
+    host: str = config["UVICORN"]["HOST"]
+    port: int = config.getint("UVICORN", "PORT")
+
+
+@dataclass
+class RedisCfg(CfgBase):
+    host: str = config["REDIS"]["HOST"]
+    port: int = config.getint("REDIS", "PORT")
+    db: int = config.getint("REDIS", "DB")
+    password: str = config["REDIS"]["PASSWORD"]
+
+
+uvicorn = UvicornCfg()
+postgres = PostgresCfg()
+redis = RedisCfg()
--- api/main.py
+++ api/main.py
@@ -6,28 +6,16 @@ from fastapi.openapi.docs import get_swagger_ui_html
 from fastapi.security import HTTPBasicCredentials, HTTPBasic
 from starlette import status
 from starlette.responses import HTMLResponse
-
+import config
 from configuration.app import App
-
 
 app = App().app
 
 
-def get_current_username(credentials: HTTPBasicCredentials = Depends(HTTPBasic())) -> str:
-    correct_username = secrets.compare_digest(credentials.username, "Reei")
-    correct_password = secrets.compare_digest(credentials.password, "dnepr1")
-    if not (correct_username and correct_password):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect email or password",
-            headers={"WWW-Authenticate": "Basic"},
-        )
-    return credentials.username
-
-
 @app.get("/api/docs", response_class=HTMLResponse)
-async def get_docs(username: str = Depends(get_current_username)) -> HTMLResponse:
+async def get_docs() -> HTMLResponse:
     return get_swagger_ui_html(openapi_url="/api/openapi.json", title="docs")
+
 
 if __name__ == "__main__":
     uvicorn.run(app, **config.uvicorn.dict())
